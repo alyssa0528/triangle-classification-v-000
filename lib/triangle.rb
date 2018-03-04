4ee646a2fc517adbdbbc477a9bcfb8a00783b444
Alyssa Kim <alyssa.kim@gmail.com>
2018-03-04 15:59:28 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,28 @@
+require 'pry'
+
 class Triangle
   # write code here
-end
+  def initialize(a, b, c)
+    @a = a 
+    @b = b 
+    @c = c 
+  end 
+  
+  def kind 
+    if (@a == @b) && (@a == @c)
+      :equilateral
+    elsif (@b == @c) && (@a != @b) || (@a == @c) && (@a != @b) || (@a == @b) && (@a != @c)
+      :isosceles 
+    elsif (@a != @b) && (@a != @c) && (@b != @c)
+      :scalene
+    elsif (@a <= 0) || (@b <= 0) || (@c <= 0) || ((@a + @b) < @c) || ((@b + @c) < @a) || ((@a + @c < @b))
+    binding.pry
+      begin 
+        raise TriangleError 
+      end
+    end 
+  end
+end 
+
+class TriangleError < StandardError
+end 
